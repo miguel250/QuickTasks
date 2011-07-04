2bbfae2b19acef36dfe463ad2a3628d3a7853f45
Miguel Perez <miguel250@gmail.com>
2011-07-03 21:12:44 -0400

    better way to handle form request

--- Views/Home.php
+++ Views/Home.php
@@ -5,7 +5,7 @@
 <title>Quick Task</title>
 </head>
 <body>
-	<form name="input" action="#" method="post">
+	<form name="input" action="/" method="post">
 		<input type="text" name="task" /> <input type="hidden" name="token"
 			value="<?php echo $data2['token'] ?>" /> <input type="submit"
 			value="Add Task" />
--- Views/Single.php
+++ Views/Single.php
@@ -10,7 +10,7 @@
 		echo '<li>'.$task['b'].'<input type="checkbox" name="completed" value="'.$task['c'].'" /></li>';
 	}?>
 	</ul>
-	<form name="input" action="#" method="post">
+	<form name="input" action="/<?php echo $data['id']?>" method="post">
 		<input type="text" name="task" /> <input type="hidden" name="token"
 			value="<?php echo $data2['token'] ?>" /> <input type="submit"
 			value="Add Task" />
